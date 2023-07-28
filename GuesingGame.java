fa5ced1a3464f20cdebe8d4cd017bacdab60268f
PatilSamruddhi02 <126335228+PatilSamruddhi02@users.noreply.github.com>
2023-07-28 11:19:12 +0530

    Add files via upload

---
+++ GuesingGame.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class GuesingGame {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        int numberToGuess = rand.nextInt(100) + 1;
+        int guess = 0;
+        int numGuesses = 0;
+        
+        System.out.println("Guess a number between 1 and 100!");
+        
+        while (guess != numberToGuess) {
+            guess = input.nextInt();
+            numGuesses++;
+            
+            if (guess < numberToGuess) {
+                System.out.println("Too low! Guess again.");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high! Guess again.");
+            }
+        }
+        
+        System.out.println("Congratulations! You guessed the number in " + numGuesses + " guesses.");
+    }
+}
